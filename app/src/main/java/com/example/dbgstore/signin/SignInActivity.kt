0bfa787ecbff796f0cf34fd24802807c909fe63e
Pranarendra08 <pranarendra@gmail.com>
2022-10-02 11:26:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dbgstore/signin/SignInActivity.kt
@@ -0,0 +1,16 @@
+package com.example.dbgstore.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.dbgstore.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+    }
+}
