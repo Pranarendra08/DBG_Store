c78e2df3999faa9df7727832a823ff5dc29e6ad1
Pranarendra08 <pranarendra@gmail.com>
2022-10-05 17:27:08 +0700

    update bottom navigation

--- app/src/main/java/com/example/dbgstore/MainActivity.kt
+++ app/src/main/java/com/example/dbgstore/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.dbgstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.example.dbgstore.databinding.ActivityMainBinding
+import com.example.dbgstore.fragment.home.HomeFragment
+import com.example.dbgstore.fragment.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +24,37 @@ class MainActivity : AppCompatActivity() {
 
     private fun initView() {
         with(binding) {
-            //val navHostfragment = supportFragmentManager
+            val fragmentHome = HomeFragment()
+            val fragmentProfile = ProfileFragment()
+
+            setFragment(fragmentHome)
+
+            ivHome.setOnClickListener {
+                setFragment(fragmentHome)
+
+                chageIcon(ivHome, R.drawable.ic_home_active)
+                chageIcon(ivProfile, R.drawable.ic_profile)
+            }
+
+            ivProfile.setOnClickListener {
+                setFragment(fragmentProfile)
+
+                chageIcon(ivHome, R.drawable.ic_home)
+                chageIcon(ivProfile, R.drawable.ic_profile_active)
+            }
         }
     }
+
+    private fun chageIcon(imageView: ImageView, int: Int) {
+        imageView.setImageResource(int)
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.layout_home, fragment)
+        fragmentTransaction.commit()
+    }
+
+
 }
